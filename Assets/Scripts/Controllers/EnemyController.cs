4bd8e6e35ef16d3ecd17240080ddc0c0fd48d44b
kitaharadaisuke <daisukekitahara0122@gmail.com>
2021-10-21 11:51:56 +0900

    [#670] 攻撃処理
    ビーム全てでダメージを与えられるように

--- Assets/Scripts/Controllers/EnemyController.cs
+++ Assets/Scripts/Controllers/EnemyController.cs
@@ -31,7 +31,21 @@ public class EnemyController : MonoBehaviour
 
     private void OnParticleCollision(GameObject other)
     {
-        hp -= 30;
+        //特殊ビーム1
+        if (other.gameObject.CompareTag("Straight"))
+        {
+            hp -= 30;
+        }
+        //特殊ビーム2
+        if (other.gameObject.CompareTag("Diffusion"))
+        {
+            hp -= 10;
+        }
+        //特殊ビーム3
+        if (other.gameObject.CompareTag("Dome"))
+        {
+            hp -= 5;
+        }
     }
 
     IEnumerator EnemyDead()
--- Assets/Scripts/Managers/EnemyDestroy.cs
+++ Assets/Scripts/Managers/EnemyDestroy.cs
@@ -18,7 +18,7 @@ public class EnemyDestroy : MonoBehaviour
 
     IEnumerator EnemyDead()
     {
-        yield return new WaitForSeconds(2.0f);
+        yield return new WaitForSeconds(3.0f);
         Destroy(this.gameObject);
     }
 }
